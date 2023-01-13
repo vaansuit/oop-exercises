f88e2f1c1dd69491133722534158fa2f612843b3
vaansuit <vvansuit@outlook.com>
2023-01-12 22:40:08 -0300

    exercise 1

---
+++ E1/Exercise1/src/People.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class People {
+
+    private String name;
+    private Date birthDate;
+    private double height;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void calculateAge(Date birthDate) {
+        
+    }
+}
