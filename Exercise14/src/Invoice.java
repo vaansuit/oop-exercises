227503a632d04bb7acca8f9c631561e9492b9e39
vaansuit <vvansuit@outlook.com>
2023-01-16 15:15:06 -0300

    Exercise14

--- Exercise14/src/Invoice.java
+++ Exercise14/src/Invoice.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Invoice {
+public class Invoice {
+
+    private String invoiceNum;
+    private String invoiceDesc;
+    private int itemQuantity;
+    private double itemCost;
+
+    public Invoice(String invoiceNum, String invoiceDesc, int itemQuantity, double itemCost) {
+        this.invoiceNum = invoiceNum;
+        this.invoiceDesc = invoiceDesc;
+        this.itemQuantity = itemQuantity;
+        this.itemCost = itemCost;
+    }
+
+    public String getInvoiceNum() {
+        return invoiceNum;
+    }
+
+    public void setInvoiceNum(String invoiceNum) {
+        this.invoiceNum = invoiceNum;
+    }
+
+    public String getInvoiceDesc() {
+        return invoiceDesc;
+    }
+
+    public void setInvoiceDesc(String invoiceDesc) {
+        this.invoiceDesc = invoiceDesc;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public double getItemCost() {
+        return itemCost;
+    }
+
+    public void setItemCost(double itemCost) {
+        this.itemCost = itemCost;
+    }
+
+    public void getTotalInvoice() {
+        double totalInvoice = itemQuantity * itemCost;
+        System.out.println(totalInvoice);
+
+    }
 }
---
+++ Exercise14/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Invoice invoice = new Invoice("1", "Description", 3, 4.50);
+
+        invoice.getTotalInvoice();
+    }
+}
